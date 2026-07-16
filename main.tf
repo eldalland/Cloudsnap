8cff4f872fdb06a5d40b56987e65cba5c8a342e5
eldalland <eldalland@gmail.com>
2026-07-16 11:53:00 -0400

    Fix: correctly connfigured log group for api in tf

--- main.tf
+++ main.tf
@@ -548,6 +548,11 @@ resource "aws_apigatewayv2_integration" "query_integration" {
   payload_format_version = "2.0"
 }
 
+resource "aws_cloudwatch_log_group" "api_logs" {
+  name              = "/aws/apigateway/cloudsnap-api"
+  retention_in_days = 7
+}
+
 # API Stage (default)
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.cloudsnap_api.id
